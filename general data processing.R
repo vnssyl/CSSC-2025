0d57bbbb14dfb80b68d31794ac6209f86fcac425
vnssyl <vanessa.y.liao@gmail.com>
2025-04-23 16:49:42 -0400

    data processing script - converting booleans to factors

---
+++ general data processing.R
@@ -0,0 +1,24 @@
+library(tidyverse)
+
+# load original dataset
+path <- "../"
+filename <- "synthetic_data_stats_competition_2025_final.xlsx"
+
+data <- read_excel(paste(path, filename, sep=""), sheet = "in")
+
+# load data dictionary
+filename <- "Data dictionary_stats competition 2025_final.xlsx"
+data_dict <- read_excel(paste(path, filename, sep=""))
+
+# convert boolean variables to factors
+cat_vars <- data_dict %>%
+  filter(`Variable type` == "boolean") %>%
+  pull(`Variable name`)
+
+data_clean <- data
+
+for (var in cat_vars) {
+  data_clean[[var]] <- as.factor(data_clean[[var]])
+}
+
+write.csv(data_clean, paste(path, 'cleaned_CSSC_data.csv', sep=""))
